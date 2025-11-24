d6278dfc64f85a94b714b9f43486bab14bdabdf0
webr1 <mkubaev24@gmail.com>
2025-11-24 23:02:21 +0500

    router added

---
+++ backend/app/routes/products.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter,Depends,status
+from sqlalchemy.orm import Session
+from typing import List
+from ..database import get_db
+from services.category_service import ProductService
+from  ..schemas.category import ProductResponse,ProductListResponse
+
+
+
+router = APIRouter(
+    prefix="/api/products",
+    tags=["products"]
+)
+
+
+@router.get("",response_model=ProductListResponse,status_code=status.HTTP_200_OK)
+def get_products(db: Session = Depends(get_db)):
+    service = ProductService(db)
+    return service.get_all_products()
+
+@router.get("/{product_id}",response_model=ProductResponse, status_code=status.HTTP_200_OK)
+def get_product_by_catrgory(category_id: int, db:Session = Depends(get_db)):
+    service = ProductService(db)
+    return service.get_products_by_id(category_id)
+
+
+@router.get("/category/{category_id}",response_model=ProductListResponse,status_code=status.HTTP_200_OK)
+def get_products_by_category(category_id: int, db:Session=Depends(get_db)):
+    service = ProductService(db)
+    return service.get_products_by_category(category_id)
